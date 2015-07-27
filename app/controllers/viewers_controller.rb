1d9989d0e8c05b40cf60693136f7aa81f19754ad
jyuusawajyuu <929503676@qq.com>
2015-07-27 01:31:41 +0000

    viewer debug

--- app/controllers/viewers_controller.rb
+++ app/controllers/viewers_controller.rb
@@ -5,10 +5,13 @@ class ViewersController < ApplicationController
   def create
     @viewer = Viewer.new(viewer_params)
     @path = @viewer.keywd
-    File.open("#{@path}","r") do |f|
+    if File.exists?("#{@path}")
+      f = File.new("#{@path}","r")
       s = f.size
       @content = f.sysread(s)
       render 'show'
+    else
+      render "result"
     end
   end
   
